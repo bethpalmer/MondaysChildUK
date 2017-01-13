b3908731afefee33995fdbe672d5ae6e482c4c9d
Beth Palmer <Beth@MacBook-Pro.local>
2017-01-13 20:41:08 +0000

    fix anchoring on accordian panels

--- js/responsive.js
+++ js/responsive.js
@@ -1,14 +1,15 @@
     // Show all content of panels on mobile
-$(window).resize(function() {
-	if ($(window).width() < 768) {
-        $(".panel").children().removeClass("collapse");
-    }
-    else {
-        $(".panel").children().addClass("collapse");   
-        location.reload();
-    }
-})    
+// $(window).resize(function() {
+// 	if ($(window).width() < 768) {
+//         $(".panel").children().removeClass("collapse");
+//     }
+//     else {
+//         $(".panel").children().addClass("collapse");   
+//         location.reload();
+//     }
+// })    
 
-    // if (document.documentElement.clientWidth < 768) 
-
-    // if (document.documentElement.clientWidth >=768) 
+$("#accordian").children().on("click","a", function() {
+    window.location.assign("#about");
+    location.reload(true);
+})
