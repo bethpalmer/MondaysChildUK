076852a390c35d134bc5bd1a1197fdb9c3b30e59
Beth Palmer <Beth@MacBook-Pro.local>
2017-01-31 11:31:19 +0000

    change fixed wall background. Add opacity backgrounds. Amend location of gallery buttons

--- js/index.js
+++ js/index.js
@@ -58,6 +58,42 @@ var openPhotoSwipe1 = function() {
             w: 500,
             h: 500,
             alt: 'Collection of craft models made at a workshop'
+        },
+        {
+            src: 'img/galleryCourses/drama-group.jpg',
+            w: 320,
+            h: 240,
+            alt: 'Mid-production picture'
+        },
+        {
+            src: 'img/galleryCourses/drama-pair.jpg',
+            w: 500,
+            h: 500,
+            alt: 'Mid-practice picture'
+        },
+        {
+            src: 'img/galleryCourses/drama-show.jpg',
+            w: 559,
+            h: 416,
+            alt: 'Mid-production picture'
+        },
+        {
+            src: 'img/galleryCourses/mask1.jpg',
+            w: 500,
+            h: 500,
+            alt: 'Child wearing hand made mask'
+        },
+        {
+            src: 'img/galleryCourses/mask2.jpg',
+            w: 500,
+            h: 500,
+            alt: 'Child wearing hand made mask'
+        },
+        {
+            src: 'img/galleryCourses/painting1.jpg',
+            w: 500,
+            h: 500,
+            alt: 'Hand made mask workshop'
         }
     ];
     
@@ -114,31 +150,91 @@ var openPhotoSwipe2 = function() {
             w: 500,
             h: 500,
             alt: '3 Peaks Challenge group shot'
+        },
+        {
+            src: 'img/galleryFundraising/arch-start.jpg',
+            w: 500,
+            h: 500,
+            alt: 'Starting position at arch'
+        },
+        {
+            src: 'img/galleryFundraising/arch-van.jpg',
+            w: 500,
+            h: 500,
+            alt: 'Supplies van'
+        },
+        {
+            src: 'img/galleryFundraising/ben-nevis.jpg',
+            w: 500,
+            h: 500,
+            alt: 'Ben Nevis path'
+        },
+        {
+            src: 'img/galleryFundraising/boot.jpg',
+            w: 629,
+            h: 500,
+            alt: 'Group shot'
+        },
+        {
+            src: 'img/galleryFundraising/boot2.jpg',
+            w: 500,
+            h: 366,
+            alt: 'Group shot'
+        },
+        {
+            src: 'img/galleryFundraising/boot3.jpg',
+            w: 500,
+            h: 339,
+            alt: 'Group shot'
+        },
+        {
+            src: 'img/galleryFundraising/bridge.jpg',
+            w: 500,
+            h: 500,
+            alt: 'Bridge shot'
+        },
+        {
+            src: 'img/galleryFundraising/carpark.jpg',
+            w: 500,
+            h: 500,
+            alt: 'Group shot'
+        },
+        {
+            src: 'img/galleryFundraising/dover.jpg',
+            w: 500,
+            h: 382,
+            alt: 'Group shot at Dover Marina'
+        },
+        {
+            src: 'img/galleryFundraising/boot2.jpg',
+            w: 500,
+            h: 403,
+            alt: 'Group shot at hospital'
+        },
+        {
+            src: 'img/galleryFundraising/loch.jpg',
+            w: 449,
+            h: 500,
+            alt: 'Shot of Loch en route'
+        },
+        {
+            src: 'img/galleryFundraising/mountain-shot.jpg',
+            w: 500,
+            h: 500,
+            alt: 'On the mountain'
+        },
+        {
+            src: 'img/galleryFundraising/pub.jpg',
+            w: 500,
+            h: 500,
+            alt: 'Well deserved beer'
+        },
+        {
+            src: 'img/galleryFundraising/van.jpg',
+            w: 500,
+            h: 288,
+            alt: 'Group shot in van on way'
         }
-        // {
-        //     src: 'img/courses/card.jpg',
-        //     w: 500,
-        //     h: 500,
-        //     alt: 'Thank you card following drama course'
-        // },
-        // {
-        //     src: 'img/courses/masks.jpg',
-        //     w: 500,
-        //     h: 500,
-        //     alt: 'Drama group in hand made masks'
-        // },
-        // {
-        //     src: 'img/courses/thankyou.jpg',
-        //     w: 500,
-        //     h: 500,
-        //     alt: 'Thank you letter following drama course'
-        // },
-        // {
-        //     src: 'img/courses/models.jpg',
-        //     w: 500,
-        //     h: 500,
-        //     alt: 'Collection of craft models made at a workshop'
-        // }
     ];
     
     // define options (if needed)
